0ec9ebb44327e34017c5c8ceac6af50917f7af91
yuto ichinose <yuto.0526.sano@gmail.com>
2022-11-25 13:19:57 +0900

    ポップオーバー

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,13 +8,25 @@ import {
   Input,
   InputGroup,
   InputLeftElement,
+  Link,
+  Popover,
+  PopoverArrow,
+  PopoverBody,
+  PopoverCloseButton,
+  PopoverContent,
+  PopoverHeader,
+  PopoverTrigger,
   Spacer,
+  Stack,
   Text,
 } from "@chakra-ui/react";
-import Link from "next/link";
 import React from "react";
 
-export default function Header() {
+type HeaderProps = {
+  isSearch?: boolean;
+};
+
+const Header: React.FC<HeaderProps> = ({ isSearch = true }) => {
   return (
     <Container
       as="header"
@@ -29,23 +41,48 @@ export default function Header() {
     >
       <HStack w="full" h="full">
         <Heading as="h1" size="md" cursor="pointer" flex={2}>
-          <Link href="/" passHref>
+          <Link href="/" _hover={{textDecoration:"none"}}>
             <Text textAlign="center" color="white">
               Markdown Blog
             </Text>
           </Link>
         </Heading>
         <Spacer flex={3} />
-        <InputGroup flex={4}>
-          <InputLeftElement pointerEvents="none">
-            <SearchIcon color="gray.300" />
-          </InputLeftElement>
-          <Input type="search" placeholder="検索" bg="white" />
-        </InputGroup>
+        {isSearch ? (
+          <InputGroup flex={4}>
+            <InputLeftElement pointerEvents="none">
+              <SearchIcon color="gray.300" />
+            </InputLeftElement>
+            <Input type="search" placeholder="検索" bg="white" />
+          </InputGroup>
+        ) : (
+          <Spacer flex={4} />
+        )}
         <Box flex={2} textAlign="center">
-          <Button colorScheme="green">新規登録・ログイン</Button>
+          <Popover>
+            <PopoverTrigger>
+              <Button colorScheme="green">新規登録・ログイン</Button>
+            </PopoverTrigger>
+            <PopoverContent w="52">
+              {/* <PopoverArrow /> */}
+              <PopoverCloseButton />
+              {/* <PopoverHeader>メニュー</PopoverHeader> */}
+              <PopoverBody>
+                <Stack>
+                  <Link href="/signup" color="teal.500">
+                    新規作成
+                  </Link>
+                  <Link href="/login" color="teal.500">
+                    ログイン
+                  </Link>
+                </Stack>
+              </PopoverBody>
+            </PopoverContent>
+          </Popover>
         </Box>
       </HStack>
     </Container>
   );
-}
+};
+
+export default Header;
