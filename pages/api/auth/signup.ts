730d15669708f5de303c9ae9d854651bcf7596b7
yuto ichinose <yuto.0526.sano@gmail.com>
2022-11-28 13:17:58 +0900

    firebase authでアカウント作成成功

--- pages/api/auth/signup.ts
+++ pages/api/auth/signup.ts
@@ -1,7 +1,9 @@
 import { addDoc, collection } from "firebase/firestore";
 import type { NextApiRequest, NextApiResponse } from "next";
-import { db } from "../../../Firebase/firebase";
+import { app, db } from "../../../Firebase/firebase";
 import { User } from "../../../models/typeUser";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth"
+
 
 // curl -X POST -H "Content-Type: application/json" -d '@requestsignupuser.json' http://localhost:3000/api/user/signup
 
@@ -22,6 +24,9 @@ export default async function handler(
         email: req.body.email,
         password: req.body.password
     }
+
+    const auth = getAuth(app)
+    await createUserWithEmailAndPassword(auth, req.body.email, req.body.password) // ここでアカウント作成
 
     const user = await addDoc(collection(db, "users"), data);
     console.log(user.id);
