255a72242b0cb43af8ccfaf94aba436912f31c00
yuto ichinose <yuto.0526.sano@gmail.com>
2022-12-16 21:17:10 +0900

    ログイン時のヘッダー追加

--- components/Header.tsx
+++ components/Header.tsx
@@ -41,7 +41,7 @@ const Header: React.FC<HeaderProps> = ({ isSearch = true }) => {
     >
       <HStack w="full" h="full">
         <Heading as="h1" size="md" cursor="pointer" flex={2}>
-          <Link href="/" _hover={{textDecoration:"none"}}>
+          <Link href="/" _hover={{ textDecoration: "none" }}>
             <Text textAlign="center" color="white">
               Markdown Blog
             </Text>
@@ -59,26 +59,52 @@ const Header: React.FC<HeaderProps> = ({ isSearch = true }) => {
           <Spacer flex={4} />
         )}
         <Box flex={2} textAlign="center">
-          <Popover>
-            <PopoverTrigger>
-              <Button colorScheme="green">新規登録・ログイン</Button>
-            </PopoverTrigger>
-            <PopoverContent w="52">
-              {/* <PopoverArrow /> */}
-              <PopoverCloseButton />
-              {/* <PopoverHeader>メニュー</PopoverHeader> */}
-              <PopoverBody>
-                <Stack>
-                  <Link href="/signup" color="teal.500">
-                    新規作成
-                  </Link>
-                  <Link href="/login" color="teal.500">
-                    ログイン
-                  </Link>
-                </Stack>
-              </PopoverBody>
-            </PopoverContent>
-          </Popover>
+          {true ? (
+            <Popover>
+              <PopoverTrigger>
+                <Button colorScheme="green">新規登録・ログイン</Button>
+              </PopoverTrigger>
+              <PopoverContent w="52">
+                {/* <PopoverArrow /> */}
+                <PopoverCloseButton />
+                {/* <PopoverHeader>メニュー</PopoverHeader> */}
+                <PopoverBody>
+                  <Stack>
+                    <Link href="/signup" color="teal.500">
+                      新規作成
+                    </Link>
+                    <Link href="/login" color="teal.500">
+                      ログイン
+                    </Link>
+                  </Stack>
+                </PopoverBody>
+              </PopoverContent>
+            </Popover>
+          ) : (
+            <Popover>
+              <PopoverTrigger>
+                <Button colorScheme="green">{'[ユーザー名]'}</Button>
+              </PopoverTrigger>
+              <PopoverContent w="52">
+                {/* <PopoverArrow /> */}
+                <PopoverCloseButton />
+                {/* <PopoverHeader>メニュー</PopoverHeader> */}
+                <PopoverBody>
+                  <Stack>
+                    <Link href="/" color="teal.500">
+                      プロフィール
+                    </Link>
+                    <Link href="/" color="teal.500">
+                      投稿一覧
+                    </Link>
+                    <Link href="/post" color="teal.500">
+                      投稿
+                    </Link>
+                  </Stack>
+                </PopoverBody>
+              </PopoverContent>
+            </Popover>
+          )}
         </Box>
       </HStack>
     </Container>
